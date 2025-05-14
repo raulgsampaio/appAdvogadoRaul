ae63688579cc2452f829addfd2aaa3d59a8f32cf
Pedro Victor Braga <pedrovictormcb80@gmail.com>
2025-05-14 16:00:49 -0300

    Trabalhando na tela de Login
    Comecei a trabalhar na estilização e na lógica de validação do e-mail e senha,
    usando o hook auth. Por algum motivo ele me retorna um erro dizendo que o auth não foi registrado, algo assim.
    Não consigo ném abrir a tela homePublic com implementação do auth

--- screens/LoginScreen.js
+++ screens/LoginScreen.js
@@ -1,22 +1,72 @@
-import React from 'react';
-import { View, Text, TouchableOpacity } from 'react-native';
-import { useNavigation } from '@react-navigation/native';
-import { loginStyles } from '../styles/loginStyles'; // Importando os estilos separados
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  Alert,
+  Image,
+  TextInput,
+} from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import auth from "../config/auth";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { loginStyles } from "../styles/loginStyles"; // Importando os estilos separados
 
 export default function LoginScreen() {
   const navigation = useNavigation();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
-  const handleLogin = () => {
-    navigation.replace('Home'); // Leva direto para Home após "login"
+  const handleLogin = async () => {
+    setLoading(true);
+
+    try {
+      await signInWithEmailAndPassword(auth, email, password);
+      Alert.alert("Login feito com sucesso!");
+      navigation.replace("Home");
+    } catch (error) {
+      Alert.alert("Erro ao fazer login", error.message);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
     <View style={loginStyles.container}>
-      <Text style={loginStyles.title}>Tela de Login</Text>
-
-      <TouchableOpacity style={loginStyles.button} onPress={handleLogin}>
-        <Text style={loginStyles.buttonText}>Entrar</Text>
-      </TouchableOpacity>
+      <View>
+        <Image
+          style={loginStyles.img}
+          source={require("../assets/leandro-silva.png")}
+        />
+        <Text>Gerenciador de Processos</Text>
+      </View>
+      <View>
+        <TextInput
+          placeholder="Digite seu e-mail"
+          value={email}
+          onChangeText={setEmail}
+          style={styles.input}
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          placeholder="Digite sua senha"
+          value={password}
+          onChangeText={setPassword}
+          style={styles.input}
+          secureTextEntry
+        />
+        <TouchableOpacity
+          style={[styles.button, loading && styles.buttonDisabled]}
+          onPress={handleLogin}
+          disabled={loading}
+        >
+          <Text style={styles.buttonText}>
+            {loading ? "Entrando..." : "Entrar"}
+          </Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 }
--- styles/loginStyles.js
+++ styles/loginStyles.js
@@ -1,19 +1,21 @@
-import { StyleSheet } from 'react-native';
-import { COLORS } from '../constants/colors';
+import { StyleSheet } from "react-native";
+import { COLORS } from "../constants/colors";
 
 export const loginStyles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: COLORS.background,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
+    // justifyContent: "center",
+    alignItems: "center",
+    // padding: 20,
+    display: "flex",
+    rowGap: 50,
   },
   title: {
     fontSize: 24,
     color: COLORS.text,
     marginBottom: 40,
-    textAlign: 'center',
+    textAlign: "center",
   },
   button: {
     backgroundColor: COLORS.primary,
@@ -21,9 +23,29 @@ export const loginStyles = StyleSheet.create({
     paddingHorizontal: 24,
     borderRadius: 8,
   },
+  buttonDisabled: {
+    backgroundColor: COLORS.unableButton,
+  },
   buttonText: {
     color: COLORS.card,
     fontSize: 16,
-    fontWeight: 'bold',
+    fontWeight: "bold",
+  },
+  input: {
+    backgroundColor: COLORS.card,
+    padding: 12,
+    borderRadius: 8,
+    marginBottom: 15,
+    fontSize: 16,
+    borderColor: COLORS.gray,
+    borderWidth: 1,
+  },
+  img: {
+    width: 150,
+    height: 150,
+    borderRadius: 100,
+    borderWidth: 5,
+    borderColor: COLORS.primary,
+    // backgroundColor: '#e1f7fa',
   },
 });
